e0393ccf9972d8f87e21278deada8ffebad3f38a
Hernán Valdés <hfvaldesg@gmail.com>
2021-02-03 11:15:32 +0100

    Build colcon_ws and bridge_ws

--- get_detection.sh
+++ get_detection.sh
@@ -9,20 +9,29 @@ mkdir bridge_ws
 mkdir colcon_ws
 mkdir catkin_ws
 
-# Clone bridge_ws (ros1_bridge) requirements
-cd bridge_ws
-mkdir src && cd src
-git clone https://github.com/h-valdes/ros1_bridge.git
-cd ../.. # go back to detection_ws
-
-# Clone catkin_ws (ROS1) requirements
-cd catkin_ws
+# Create catkin_ws (ROS1) 
+cd $HOME/detection_ws/catkin_ws
 mkdir src && cd src
 git clone https://github.com/h-valdes/detection.git
 git clone https://github.com/h-valdes/apriltag_ros
-cd ..
-gnome-terminal -- /bin/bash -c "source /opt/ros/noetic/setup.bash; catkin build; exec bash" &
-cd .. # go back to detection_ws
+git clone https://github.com/ros-drivers/openni2_camera
+cd .. # go to catkin_ws
+gnome-terminal --wait -- /bin/bash -c "source /opt/ros/noetic/setup.bash; catkin build"
 
-# Clone colcon_ws (ROS2)
-## TODO: Project infrared-tracking is in gitlab
+# Create colcon_ws (ROS2)
+cd $HOME/detection_ws/colcon_ws
+mkdir src && cd src
+git clone https://git.informatik.tu-freiberg.de/SoftwareentwicklungUndRobotik/Programming/alphabots/infrared-tracking.git
+git clone https://git.informatik.tu-freiberg.de/SoftwareentwicklungUndRobotik/Programming/alphabots/ros2/apriltag_msgs.git
+cd infrared-tracking
+git checkout ros2
+cd .. # go to colcon_ws
+gnome-terminal --wait -- /bin/bash -c "source /opt/ros/foxy/setup.bash; colcon build"
+
+# Create bridge_ws (ros1_bridge)
+cd $HOME/detection_ws/bridge_ws
+mkdir src && cd src
+git clone https://github.com/h-valdes/ros1_bridge.git
+
+# go back to detection_ws
+cd $HOME/detection_ws 
