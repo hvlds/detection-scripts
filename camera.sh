a31edfe182e389d3c8ed3f148385485337e286a8
Hernán Valdés <hfvaldesg@gmail.com>
2021-02-04 15:49:32 +0100

    Update scripts

--- camera.sh
+++ camera.sh
@@ -3,15 +3,28 @@ SCRIPTS_DIR="~/scripts"
 gnome-terminal -- /bin/bash -c "source ${SCRIPTS_DIR}/start_noetic.sh; \
 roslaunch detection only_camera.launch; \
 exec bash" &
+
 sleep 2
+
 gnome-terminal -- /bin/bash -c "source ${SCRIPTS_DIR}/start_bridge.sh; \
 ros2 run ros1_bridge static_bridge_filtered_camera; \
 exec bash" &
+
 sleep 2
+
 gnome-terminal -- /bin/bash -c "source ${SCRIPTS_DIR}/start_foxy.sh; \
 ros2 launch irtracking infrared_tracking_launch.py; \
 exec bash" &
+
 sleep 2
+
+gnome-terminal -- /bin/bash -c "source ${SCRIPTS_DIR}/start_foxy.sh; \
+ros2 topic echo /irtracking/raw_tag_detections; \
+exec bash" &
+
+sleep 2
+
 gnome-terminal -- /bin/bash -c "source ${SCRIPTS_DIR}/start_foxy.sh; \
 ros2 run rqt_image_view rqt_image_view; \
+
 exec bash" &
