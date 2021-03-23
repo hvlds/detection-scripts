94993512bfba14537951535698492059e190a276
Hernán Valdés <hfvaldesg@gmail.com>
2021-03-23 10:13:02 +0100

    camera.py: Add docstrings and fix scripts directory

--- camera.py
+++ camera.py
@@ -1,47 +1,75 @@
 import os
 import time
+import sys
 
-SCRIPTS_DIR = "~/scripts"
 
+def start_camera(exec_dir):
+    """Start the launch script for the camera Asus Xtion running in ROS(1)
+    Noetic.
 
-def start_camera():
+    Args:
+        exec_dir (str): Directory with the launch scripts for the project
+    """
     os.system(
-        f"gnome-terminal -- /bin/bash -c 'source {SCRIPTS_DIR}/start_noetic.sh;"
+        f"gnome-terminal -- /bin/bash -c '. {exec_dir}/start_noetic.sh;"
         "roslaunch detection only_camera.launch;"
         "exec bash' &")
 
 
-def start_bridge():
+def start_bridge(exec_dir):
+    """Start ros1_bridge to enable communication between ROS1 and ROS2
+
+    Args:
+        exec_dir (str): Directory with the launch scripts for the project
+    """
     os.system(
-        f"gnome-terminal -- /bin/bash -c 'source {SCRIPTS_DIR}/start_bridge.sh;"
+        f"gnome-terminal -- /bin/bash -c '. {exec_dir}/start_bridge.sh;"
         "ros2 run ros1_bridge static_bridge_filtered_camera;"
         "exec bash' &")
 
 
-def start_static_tf():
+def start_static_tf(exec_dir):
+    """Publish basic static tf broadcaster for the map->base_link relationship 
+
+    Args:
+        exec_dir (str): Directory with the launch scripts for the project
+    """
     os.system(
-        f"gnome-terminal -- /bin/bash -c 'source {SCRIPTS_DIR}/start_foxy.sh;"
+        f"gnome-terminal -- /bin/bash -c '. {exec_dir}/start_foxy.sh;"
         "ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 1 map base_link' &")
 
 
-def start_irtracking():
+def start_irtracking(exec_dir):
+    """Start the irtracking project (ROS2)
+
+    Args:
+        exec_dir (str): Directory with the launch scripts for the project
+    """
     os.system(
-        f"gnome-terminal -- /bin/bash -c 'source {SCRIPTS_DIR}/start_foxy.sh;"
+        f"gnome-terminal -- /bin/bash -c '. {exec_dir}/start_foxy.sh;"
         "ros2 launch irtracking infrared_tracking_launch.py;"
         "exec bash' &")
 
 
-def start_image_viewer():
+def start_image_viewer(exec_dir):
+    """Start the image viewer (ROS2)
+
+    Args:
+        exec_dir (str): Directory with the launch scripts for the project
+    """
     os.system(
-        f"gnome-terminal -- /bin/bash -c 'source {SCRIPTS_DIR}/start_foxy.sh;"
+        f"gnome-terminal -- /bin/bash -c '. {exec_dir}/start_foxy.sh;"
         "ros2 run rqt_image_view rqt_image_view;"
         "exec bash' &")
 
 
 if __name__ == "__main__":
-    exec_list = [start_camera, start_bridge, start_static_tf,
-                    start_irtracking, start_image_viewer]
+    exec_list = [start_camera, start_bridge, start_static_tf, 
+        start_irtracking, start_image_viewer]
+    script_path = os.path.realpath(sys.argv[0])
+    temp_dir = script_path.split("/")
+    script_dir = "/".join(temp_dir[0:-1])
 
     for step in exec_list:
-        step()
+        step(script_dir)
         time.sleep(2)
