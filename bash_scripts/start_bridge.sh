7b604b84a086668d203e61bae04509f5996a84ba
Hernán Valdés <hfvaldesg@gmail.com>
2021-03-23 15:31:00 +0100

    fix scripts

--- bash_scripts/start_bridge.sh
+++ bash_scripts/start_bridge.sh
@@ -1,4 +1,4 @@
-. start_foxy.sh
-. start_noetic.sh
+. ~/scripts/bash_scripts/start_foxy.sh
+. ~/scripts/bash_scripts/start_noetic.sh
 . ~/detection_ws/bridge_ws/install/local_setup.bash
 # . ~/bridge_ws/install/local_setup.bash
--- camera.py
+++ camera.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
         start_irtracking, start_image_viewer]
     script_path = os.path.realpath(sys.argv[0])
     temp_dir = script_path.split("/")
-    script_dir = "/".join(temp_dir[0:-1])
+    script_dir = "/".join(temp_dir[0:-1]) + "/bash_scripts"
 
     for step in exec_list:
         step(script_dir)
