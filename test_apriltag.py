c87c97a684175d278a0c67eee2f273da811984a7
Hernán Valdés <hfvaldesg@gmail.com>
2021-04-09 09:59:36 +0200

    Start running the apriltag

--- test_apriltag.py
+++ test_apriltag.py
@@ -4,9 +4,19 @@ import sys
 from camera import start_camera, start_bridge
 from utils import *
 
+def echo_apriltag(exec_dir):
+    os.system(
+        f"gnome-terminal -- /bin/bash -c '. {exec_dir}/start_foxy.sh;"
+        "ros2 topic echo /tag_detections;"
+        "exec bash' &"
+    )
+
+def start_bag(exec_dir):
+    pass
+
 
 if __name__ == "__main__":
-    exec_list = [start_camera, start_bridge]
+    exec_list = [start_camera, start_bridge, echo_apriltag]
 
     script_path = os.path.realpath(sys.argv[0])
     temp_dir = script_path.split("/")
@@ -14,4 +24,4 @@ if __name__ == "__main__":
 
     for step in exec_list:
         step(script_dir)
-        time.sleep(2)
+        time.sleep(4)
