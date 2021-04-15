d4d6990c6d88ace349d58a8d515a8d335fe1814b
Hernán Valdés <hfvaldesg@gmail.com>
2021-04-15 17:37:48 +0200

    Separate the graphs of irmarker and apriltag
    Separate graphs doesnt have place to interpretation

--- plot.py
+++ plot.py
@@ -12,36 +12,59 @@ def save_graph(x_values, y_values, x_label, y_label, output_file):
     plt.savefig(output_file, bbox_inches="tight")
     plt.clf()
 
+
 def plot_comparison(apriltag_s3_csv, apriltag_s2_csv, infrared_csv, base, output):
     apriltag_s3 = pd.read_csv(apriltag_s3_csv)
     apriltag_s2 = pd.read_csv(apriltag_s2_csv)
     infrared = pd.read_csv(infrared_csv)
 
     components = ["x", "y", "z"]
-    for component in components:
+    for index, component in enumerate(components):
+        # Plot Apriltag graphs
         apriltag_time = apriltag_s3["time"]
         apriltag_comp = apriltag_s3[component]
-        plt.plot(apriltag_time, apriltag_comp, label = "apriltag_s3")
+        plt.plot(apriltag_time, apriltag_comp, label="apriltag_s3")
 
         apriltag_time = apriltag_s2["time"]
         apriltag_comp = apriltag_s2[component]
-        plt.plot(apriltag_time, apriltag_comp, label = "apriltag_s2")
-
-        infrared_time = infrared["time"]
-        infrared_comp = infrared[component]
-        plt.plot(infrared_time, infrared_comp, label = "infrared")
+        plt.plot(apriltag_time, apriltag_comp, label="apriltag_s2")
 
         if component == "z":
-            plt.axhline(y=base, color='r', linestyle='-', label="Groundtruth")
+            base_apriltag = base[index] + 6
+            plt.axhline(y=base_apriltag, color='r', linestyle='-',
+                        label="Ground Truth Apriltag")
 
         plt.xlabel("Time [s]")
         plt.ylabel(f"Position in {component} [cm]")
 
         plt.legend()
-        plt.savefig(f"{output}_{component}.png", bbox_inches="tight")
+        plt.savefig(f"{output}_apriltag_{component}.png", bbox_inches="tight")
+
+        plt.clf()
+
+        # Plot IRMarker graphs
+        infrared_time = infrared["time"]
+        infrared_comp = infrared[component]
+        plt.plot(infrared_time, infrared_comp, label="infrared")
+
+
+        if component == "z":
+            plt.axhline(y=base[index], color='r',
+                        linestyle='-', label="Ground Truth IRMarker")
+
+        plt.xlabel("Time [s]")
+        plt.ylabel(f"Position in {component} [cm]")
+
+        plt.legend()
+        plt.savefig(f"{output}_irmarker_{component}.png", bbox_inches="tight")
 
         plt.clf()
 
 
 if __name__ == "__main__":
-    pass
+    plot_comparison(
+        "./results/apriltag_test_random.csv",
+        "./results/apriltag_test_random.csv",
+        "./results/infrared_test3.csv",
+        [68, 68, 68],
+        "random")
