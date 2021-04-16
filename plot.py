302e3b73af02afc1fc68c75f31d2c45ee1788db7
Hernán Valdés <hfvaldesg@gmail.com>
2021-04-16 17:51:45 +0200

    plot: fix the diagrams

--- plot.py
+++ plot.py
@@ -13,14 +13,12 @@ def save_graph(x_values, y_values, x_label, y_label, output_file):
     plt.clf()
 
 
-def plot_comparison(apriltag_s3_csv, apriltag_s2_csv, infrared_csv, base, output):
+def plot_apriltag(apriltag_s3_csv, apriltag_s2_csv, base, output):
     apriltag_s3 = pd.read_csv(apriltag_s3_csv)
     apriltag_s2 = pd.read_csv(apriltag_s2_csv)
-    infrared = pd.read_csv(infrared_csv)
 
     components = ["x", "y", "z"]
     for index, component in enumerate(components):
-        # Plot Apriltag graphs
         apriltag_time = apriltag_s3["time"]
         apriltag_comp = apriltag_s3[component]
         plt.plot(apriltag_time, apriltag_comp, label="apriltag_s3")
@@ -29,42 +27,53 @@ def plot_comparison(apriltag_s3_csv, apriltag_s2_csv, infrared_csv, base, output
         apriltag_comp = apriltag_s2[component]
         plt.plot(apriltag_time, apriltag_comp, label="apriltag_s2")
 
-        if component == "z":
-            base_apriltag = base[index] + 6
-            plt.axhline(y=base_apriltag, color='r', linestyle='-',
-                        label="Ground Truth Apriltag")
+        plt.axhline(y=base[index], color='g', linestyle='-',
+                    label="Ground Truth Apriltag")
 
-        plt.xlabel("Time [s]")
-        plt.ylabel(f"Position in {component} [cm]")
+        plt.xlabel("Zeit [s]")
+        plt.ylabel(f"Position in {component.upper()}-Richtung [cm]")
 
-        plt.legend()
+        plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
         plt.savefig(f"{output}_apriltag_{component}.png", bbox_inches="tight")
 
         plt.clf()
 
-        # Plot IRMarker graphs
-        infrared_time = infrared["time"]
-        infrared_comp = infrared[component]
-        plt.plot(infrared_time, infrared_comp, label="infrared")
 
+def plot_infrared(infrared_1_csv, infrared_2_csv, base, output):
+    infrared_1 = pd.read_csv(infrared_1_csv)
+    infrared_2 = pd.read_csv(infrared_2_csv)
 
-        if component == "z":
-            plt.axhline(y=base[index], color='r',
-                        linestyle='-', label="Ground Truth IRMarker")
+    components = ["x", "y", "z"]
+    for index, component in enumerate(components):
+        infrared_time = infrared_1["time"]
+        infrared_comp = infrared_1[component]
+        plt.plot(infrared_time, infrared_comp, label="infrared_1")
 
-        plt.xlabel("Time [s]")
-        plt.ylabel(f"Position in {component} [cm]")
+        infrared_time = infrared_2["time"]
+        infrared_comp = infrared_2[component]
+        plt.plot(infrared_time, infrared_comp, label="infrared_2")
 
-        plt.legend()
+        plt.axhline(y=base[index], color="g",
+                    linestyle='-', label="Ground Truth IR-Marker")
+
+        plt.xlabel("Zeit [s]")
+        plt.ylabel(f"Position in {component.upper()}-Richtung [cm]")
+
+        plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
         plt.savefig(f"{output}_irmarker_{component}.png", bbox_inches="tight")
 
         plt.clf()
 
 
 if __name__ == "__main__":
-    plot_comparison(
-        "./results/apriltag_test_random.csv",
-        "./results/apriltag_test_random.csv",
-        "./results/infrared_test3.csv",
-        [68, 68, 68],
-        "random")
+    plot_apriltag(
+        "./results/apriltag_test3_s3.csv",
+        "./results/apriltag_test3_s2.csv",
+        [2.2, -2.2, 170],
+        "apriltag_test3")
+    
+    plot_infrared(
+        "./results/infrared_test3_1.csv",
+        "./results/infrared_test3_2.csv",
+        [4.8, -2.2, 165],
+        "irmarker_test3")
